b067bcb98c14bac8560e199cd1d3ac1a722ab336
Utkarsh Singh <utkarsh.singh9729@gmail.com>
2023-08-29 22:50:33 +0530

    initial

---
+++ Backend/copdo/copdo/src/main/java/com/core/copdo/CopdoApplication.java
@@ -0,0 +1,13 @@
+package com.core.copdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopdoApplication.class, args);
+	}
+
+}
---
+++ Backend/copdo/copdo/src/test/java/com/core/copdo/CopdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.copdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
